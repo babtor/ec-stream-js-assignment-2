aa48b3fa2c6370ff6618bbf64504e7ff8cf4c859
babtor <lucaswikstrom96@gmail.com>
2023-06-12 12:40:39 +0200

    Implemented checkboxes for ticking off finished tasks

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 function createTasks() {
   const userInput = document.getElementById("userInput").value;
   const taskText = document.createElement("div");
+  const container = document.getElementById("taskContainer");
   taskText.textContent = userInput;
 
-  const container = document.getElementById("todoContainer");
+  const createCheckBox = document.createElement("input");
+  createCheckBox.type = "checkBox";
+
+  createCheckBox.addEventListener("change", function () {
+    if (createCheckBox.checked) {
+      console.log("Checkbox is checked");
+      createCheckBox.style.accentColor = "green";
+    } else {
+      console.log("Checkbox is unchecked");
+    }
+  });
+
   container.appendChild(taskText);
+  taskText.appendChild(createCheckBox);
 
   fetch("https://dummyjson.com/todos/add", {
     method: "POST",
